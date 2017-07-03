ea8e7695bdcc43c156b509489bdfc06f2cd1eaa8
Jerry Goguette <jerry_g16@hotmail.com>
2017-07-03 17:06:21 -0400

    Building and using DropDown Directive Managing Recipes in a Recipe Service

---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,18 @@
+import {Directive, HostBinding, HostListener, OnInit} from '@angular/core';
+
+@Directive({
+  selector: '[appDropdown]'
+})
+export class DropdownDirective implements OnInit {
+  @HostBinding('class.open') isOpen = false;
+
+  @HostListener('click') toggleOpen() {
+      this.isOpen = !this.isOpen;
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+}
